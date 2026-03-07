42a6217d5173af29767603fc7df61ef5fc267f76
hyperionliuziqi <2683476359@qq.com>
2026-03-07 17:50:46 +0800

    第三个修改

--- test.py
+++ test.py
@@ -4,3 +4,9 @@ c=3
 print(f"a+b+c = {a+b+c}")
 d=a*b*c
 print(f"d = {d}")
+from manim import *
+class Test(Scene):
+    def construct(self):
+        text = Text("Hello, Manim!")
+        self.play(Write(text))
+        self.wait(2)
