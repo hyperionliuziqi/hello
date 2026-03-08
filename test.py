caea72bfd3416e42ce77c924d0872dd667abacc4
hyperionliuziqi <2683476359@qq.com>
2026-03-08 11:30:50 +0800

    第6次

--- test.py
+++ test.py
@@ -3,4 +3,10 @@ class Test(Scene):
     def construct(self):
         circle = Circle()
         self.play(Create(circle))
+        q=Text("Fuck you")
+        self.play(Write(q))
+        square = Square().shift(LEFT*2)
+        triangle = Triangle().shift(RIGHT*2)
+        self.play(Create(square))
+        self.play(Create(triangle))
         self.wait(1)
